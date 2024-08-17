4995b4ff00789868f332cfaef4f5495003ab20cd
Lor <lorelailyons@protonmail.com>
2024-08-17 00:25:06 -0400

    Now nothing is working but I uncovered some errors and so I need to push them to get help fixing this.

--- password manager/src/LPM/clj/routes.clj
+++ password manager/src/LPM/clj/routes.clj
@@ -1,10 +1,11 @@
 (ns LPM.clj.routes
-  (:require [compojure.core :refer [defroutes POST]]
+  (:require [compojure.core :refer [defroutes POST GET DELETE]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.middleware.cors :refer [wrap-cors]]
             [clojure.data.json :as cjson]
             [LPM.clj.user :as usr]
+            [LPM.clj.pwfuncs :as pwf]
             [clojure.tools.logging :as log]))
 
 (defroutes app-routes
@@ -16,6 +17,7 @@
       {:status 200
        :headers {"Content-Type" "application/json"}
        :body (cjson/write-str {:message "User created successfully"})}))
+  
   (POST "/add-a-new-password" {:keys [body]}
     (let [profile-name (:userProfileName body)
           login-password (:userLoginPassword body)
@@ -24,12 +26,24 @@
       (usr/add-new-password profile-name login-password passwords)
       {:status 200
        :headers {"Content-Type" "application/json"}
-       :body (cjson/write-str {:message "L-> Password added successfully"})})))
+       :body (cjson/write-str {:message "L-> Password added successfully"})}))
+  
+  (DELETE "/remove-a-password" {:keys [body]}
+    (let [profile-name (:userProfileName body)
+          pName (:pName body)]
+      (log/info "L->Handling password removal" body)
+      (usr/remove-a-password profile-name pName)
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body (cjson/write-str {:message "L-> Password removed successfully"})}))
+  
+  (GET "/generate-a-password" [size]
+    (pwf/generate-password size)))
 
 (def handler
   (-> app-routes
       (wrap-cors :access-control-allow-origin  #".*"
-                 :access-control-allow-methods [:get :post :options])
+                 :access-control-allow-methods [:get :post :options :delete])
       (wrap-json-body)
       (wrap-json-response)))
 
