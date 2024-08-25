905be7cd0f8c9f348b141eaf7474236768594bf5
Lor <lorelailyons@protonmail.com>
2024-08-25 01:05:43 -0400

    Added an asynchronous loading icon to a couple places and ensured that the setup screen does not flash every time you open the application.

--- password manager/src/LPM/cljs/core.cljs
+++ password manager/src/LPM/cljs/core.cljs
@@ -12,36 +12,54 @@
 
 (defn main-page-input-container []
   (let [selected-file (r/atom nil)
-        error-message (r/atom "") 
-        update-setup-complete (fn [status] (reset! help/setup-complete status))]
+        error-message (r/atom "")]
     (fn []
-      (if (not @help/setup-complete)
-        (do
-          (help/check-setup update-setup-complete)
-          [isup/key-setup-component])
-        [:div
-         (if @help/logged-in
-           [home/logged-in-view]
-           [:div.main-container {:style {:margin-bottom (if @help/logged-in "5vh" "10vh")}}
-            [home/heading-box]
-            [:h2 {:style {:margin-bottom "2vh" :margin-top "3vh"}} "Login"]
-            [lgn/directory-box
-             (fn [file]
-               (reset! selected-file file))]
-            [lgn/name-and-password-input selected-file
-             (fn [profile-name password]
-               (if (or (empty? profile-name) (empty? password))
-                 (reset! error-message "Please fill in both fields.")
-                 (if (and @selected-file (and profile-name password))
-                   (do
-                     (swap! @help/user-state assoc :current-user profile-name)
-                     (reset! @help/logged-in {:loggedIn false}))
-                   (reset! error-message "Invalid profile-name or password"))))]
-            (when (not @help/logged-in)
-              [:div.error-message @error-message])])]))))
+      [:div
+       (if @help/logged-in
+         [home/logged-in-view]
+         [:div.main-container {:style {:margin-bottom (if @help/logged-in "5vh" "10vh")}}
+          [home/heading-box]
+          [:h2 {:style {:margin-bottom "2vh" :margin-top "3vh"}} "Login"]
+          [lgn/directory-box
+           (fn [file]
+             (reset! selected-file file))]
+          [lgn/name-and-password-input selected-file
+           (fn [profile-name password]
+             (if (or (empty? profile-name) (empty? password))
+               (reset! error-message "Please fill in both fields.")
+               (if (and @selected-file (and profile-name password))
+                 (do
+                   (swap! @help/user-state assoc :current-user profile-name)
+                   (reset! @help/logged-in {:loggedIn false}))
+                 (reset! error-message "Invalid profile-name or password"))))]
+          (when (not @help/logged-in)
+            [:div.error-message @error-message])])])))
 
 (defn login-page []
-  (main-page-input-container))
+  (let [loading? (r/atom true)
+        setup-complete? (r/atom false)]
+
+    (r/create-class
+     {:component-did-mount
+      (fn []
+        (help/check-setup
+         (fn [status]
+           (reset! setup-complete? status)
+           (reset! loading? false))))
+
+     :reagent-render
+     (fn []
+       (cond
+         @loading?
+         [:div.loadinggg
+          [:img {:src "/assets/loading.gif"}]
+          [:p "Generating keys..."]]
+
+         (not @setup-complete?)
+         [isup/key-setup-component]
+
+         :else
+         [main-page-input-container]))})))
 
 (defn ^:dev/after-load start []
   (rdom/render [login-page]
